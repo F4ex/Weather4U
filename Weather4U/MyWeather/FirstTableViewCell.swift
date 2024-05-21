55127961fb127cde95870cc681671e1d25ec78fa
taeng <taeng418@icloud.com>
2024-05-21 21:11:17 +0900

    FirstTableViewCell 추가
    FirstTableViewCell 추가 및 LayOut 수정

--- Weather4U/MyWeather/FirstTableViewCell.swift
+++ Weather4U/MyWeather/FirstTableViewCell.swift
@@ -1,35 +1,37 @@
 //
-//  MyWeatherPageTableViewCell.swift
+//  FirstTableViewCell.swift
 //  Weather4U
 //
-//  Created by 강태영 on 5/13/24.
+//  Created by 강태영 on 5/21/24.
 //
 
 import UIKit
 import SnapKit
-import Kingfisher
 
-class MyWeatherPageTableViewCell: UITableViewCell {
-    
+class FirstTableViewCell: UITableViewCell {
+
+    let locationLabel = UILabel()
     let cityLabel = UILabel()
     let tempLabel = UILabel()
     let highLabel = UILabel()
     let lowLabel = UILabel()
     let weatherLabel = UILabel()
-    let cellImageView = UIImageView() // imageView 이름 변경
-    
+    let weatherImageView = UIImageView()
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         
         self.backgroundColor = .white
-        
+        self.contentView.backgroundColor = UIColor(named: "Background")
         self.contentView.layer.cornerRadius = 20 // 반경을 조정하여 원하는 둥근 정도를 설정할 수 있습니다.
         self.contentView.layer.masksToBounds = true // 셀의 내용이 경계를 벗어날 때 잘라내기 설정
         
         // 폰트 및 색상 설정
+        locationLabel.textColor = UIColor(named: "font")
+        locationLabel.font = UIFont.systemFont(ofSize: 24, weight: .regular)
+        
         cityLabel.textColor = UIColor(named: "font")
-        cityLabel.font = UIFont.systemFont(ofSize: 24, weight: .regular)
+        cityLabel.font = UIFont.systemFont(ofSize: 13, weight: .regular)
         
         tempLabel.textColor = UIColor(named: "cell")
         tempLabel.font = UIFont.systemFont(ofSize: 34, weight: .semibold)
@@ -43,29 +45,31 @@ class MyWeatherPageTableViewCell: UITableViewCell {
         weatherLabel.textColor = UIColor(named: "font")
         weatherLabel.font = UIFont.systemFont(ofSize: 13, weight: .semibold)
         
-        cellImageView.image = UIImage(named: "sun2")
-        cellImageView.contentMode = .scaleAspectFit
+        weatherImageView.image = UIImage(named: "sun2")
+        weatherImageView.contentMode = .scaleAspectFit
         
-        [cityLabel, tempLabel, highLabel, lowLabel, weatherLabel, cellImageView].forEach {
+        // Add subviews to contentView
+        [locationLabel, cityLabel, tempLabel, highLabel, lowLabel, weatherLabel, weatherImageView].forEach {
             contentView.addSubview($0)
         }
         
-        
-        [cityLabel, tempLabel, highLabel, lowLabel, weatherLabel, cellImageView].forEach {
-            contentView.addSubview($0)
-        }
-        
-        
-        // Auto Layout constraints 설정
-        cityLabel.snp.makeConstraints { make in
-            make.top.equalToSuperview().inset(19)
-            make.leading.equalToSuperview().inset(20)
-            make.width.equalTo(42)
+        // 오토레이아웃 설정
+        locationLabel.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(11)
+            make.leading.equalToSuperview().offset(20)
+            make.width.equalTo(124)
             make.height.equalTo(29)
         }
         
+        cityLabel.snp.makeConstraints { make in
+            make.top.equalToSuperview().inset(38)
+            make.leading.equalToSuperview().inset(20)
+            make.width.equalTo(57)
+            make.height.equalTo(16)
+        }
+        
         tempLabel.snp.makeConstraints { make in
-            make.top.equalToSuperview().offset(57)
+            make.top.equalToSuperview().offset(63)
             make.leading.equalToSuperview().inset(20)
             make.width.equalTo(42)
             make.height.equalTo(41)
@@ -92,24 +96,22 @@ class MyWeatherPageTableViewCell: UITableViewCell {
             make.height.equalTo(16)
         }
         
-        cellImageView.snp.makeConstraints { make in
+        weatherImageView.snp.makeConstraints { make in
             make.top.bottom.equalToSuperview()
             make.leading.equalToSuperview().inset(218)
             make.width.equalTo(121)
-            make.height.equalTo(111)
+            make.height.equalTo(111) // 이미지 뷰의 높이 설정
         }
     }
-    
     
     override func layoutSubviews() {
         super.layoutSubviews()
         
         // contentView 프레임을 수정하여 패딩 추가
-        contentView.frame = contentView.frame.inset(by: UIEdgeInsets(top: 0, left:0, bottom: 5, right: 5))
+        contentView.frame = contentView.frame.inset(by: UIEdgeInsets(top: 0, left: 0, bottom: 5, right: 5))
     }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
 }
